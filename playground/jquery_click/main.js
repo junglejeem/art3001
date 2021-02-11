ba972b2e1246b828d52af57d976575c7215d4f73
Hirad Sab <hsab@users.noreply.github.com>
2021-02-11 13:40:41 -0500

    jquery click, css, toggle, fade

---
+++ playground/jquery_click/main.js
@@ -0,0 +1,20 @@
+var isBottomBlue = true;
+
+$(document).ready(function() {
+
+    $("#uniqueDiv").click(function() {
+        if (isBottomBlue === true) {
+            $(".bottom").css("background", "yellow");
+            isBottomBlue = false;
+        } else {
+            $(".bottom").css("background", "blue");
+            isBottomBlue = true;
+        }
+        console.log("Changed the value of isBottomBlue to ", isBottomBlue, " !!!")
+    });
+
+    $(".bottom").click(function() {
+        $(".top").fadeToggle(2000);
+    });
+
+});
